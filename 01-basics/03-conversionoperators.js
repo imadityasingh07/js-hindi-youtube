5aeaa021d79ba7b931badea051e5ffbab56e70ef
imadityasingh07 <adityasingh20637@gmail.com>
2025-03-05 23:42:58 +0530

    data type conversion

--- 01-basics/03-conversionoperators.js
+++ 01-basics/03-conversionoperators.js
@@ -1,24 +1,24 @@
- // let score = "33abc"
+  let score = "33abc"
 
-// let score = null // null ki value 0 hoga
+ let score = null // null ki value 0 hoga
 
- //  let score = undefined // undefined ki value undefined hoga
+   let score = undefined // undefined ki value undefined hoga
 
-//  let score = true // true ki value 1 hoga
+  let score = true // true ki value 1 hoga
 
-// let score = false // false ki value 0 hoga
+ let score = false // false ki value 0 hoga
 
- // let score = "Aditya" // string
+  let score = "Aditya" // string
 
-// console.log(typeof score);// number
-// console.log(typeof(score));// number
+ console.log(typeof score);// number
+ console.log(typeof(score));// number
 
-// let valueNumber = Number(score) // this is a capital based function
-// console.log(typeof valueNumber);// number
-// console.log(valueNumber);//33
+ let valueNumber = Number(score) // this is a capital based function
+ console.log(typeof valueNumber);// number
+ console.log(valueNumber);//33
 
- //console.log(typeof NaN); // number
-//console.log(NaN); // Nan
+ console.log(typeof NaN); // number
+console.log(NaN); // Nan
 
 // "33" => 33
 // "33abc" => Nan
@@ -29,14 +29,14 @@
 // "Aditya" => Nan
 /// this is a basic conversion operator
 
- // let isLoggedIn = 1 // 1 means true; 0 means false
+  let isLoggedIn = 1 // 1 means true; 0 means false
 
- // let isLoggedIn = "mayank" // "mayank" means true;
+  let isLoggedIn = "mayank" // "mayank" means true;
 
-// let isLoggedIn = "" // means false/ empty string
+ let isLoggedIn = "" // means false/ empty string
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn); // true
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
 
 let somenumber = 33
 
