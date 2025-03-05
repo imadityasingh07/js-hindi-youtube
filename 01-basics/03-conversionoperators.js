ae3bb2c1a29a3adbd0dc3df08e0d0ce1d8ed26af
imadityasingh07 <adityasingh20637@gmail.com>
2025-03-05 23:38:42 +0530

    conversion of datatypes

---
+++ 01-basics/03-conversionoperators.js
@@ -0,0 +1,45 @@
+ // let score = "33abc"
+
+// let score = null // null ki value 0 hoga
+
+ //  let score = undefined // undefined ki value undefined hoga
+
+//  let score = true // true ki value 1 hoga
+
+// let score = false // false ki value 0 hoga
+
+ // let score = "Aditya" // string
+
+// console.log(typeof score);// number
+// console.log(typeof(score));// number
+
+// let valueNumber = Number(score) // this is a capital based function
+// console.log(typeof valueNumber);// number
+// console.log(valueNumber);//33
+
+ //console.log(typeof NaN); // number
+//console.log(NaN); // Nan
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+// false => 0
+// null => 0
+// undefined => Nan
+// "Aditya" => Nan
+/// this is a basic conversion operator
+
+ // let isLoggedIn = 1 // 1 means true; 0 means false
+
+ // let isLoggedIn = "mayank" // "mayank" means true;
+
+// let isLoggedIn = "" // means false/ empty string
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
+
+let somenumber = 33
+
+let stringsomenumber = String(somenumber)
+console.log(stringsomenumber);
+console.log(typeof stringsomenumber);
